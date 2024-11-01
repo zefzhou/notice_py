1c7e6fa65a2adaacd0d8170f4a64bf0258c95792
zefzhou44 <zefzhou44@gmail.com>
2024-11-01 15:07:08 +0800

    init

---
+++ notice/sms.py
@@ -0,0 +1,33 @@
+from alibabacloud_dysmsapi20170525.models import SendSmsRequest
+from alibabacloud_dysmsapi20170525.client import Client
+from alibabacloud_tea_openapi.models import Config
+import json
+
+
+class SMS:
+    client: Client
+
+    def __init__(self,
+                 access_key_id: str,
+                 access_key_secret: str,
+                 region_id: str = 'dysmsapi.aliyuncs.com') -> None:
+        config = Config(access_key_id=access_key_id,
+                        access_key_secret=access_key_secret,
+                        region_id=region_id)
+        self.client = Client(config=config)
+
+    def send(self, phone_num, sign_name: str, template_code: str,
+             params: dict):
+        req = SendSmsRequest(phone_numbers=str(phone_num),
+                             sign_name=sign_name,
+                             template_code=template_code,
+                             template_param=json.dumps(params))
+        try:
+            res = self.client.send_sms(request=req)
+            data = res.to_map()
+            if data['statusCode'] >= 200 and data['statusCode'] < 300 and data[
+                    'body']['code'] == 'OK':
+                return None
+            return data['body']['Message']
+        except Exception as e:
+            return f'{e}'
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="notice_py",
+    version="0.1.0",
+    author="zefzhou44",
+    author_email="zefzhou44@gmail.com",
+    description="lark/sms/voice notice package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zefzhou/notice_py",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9.0",
+    install_requires=[
+        "requests",
+        "alibabacloud_dysmsapi20170525",
+    ],
+)
